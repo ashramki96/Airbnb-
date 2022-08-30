78d8fbc997b24cd0ca2b17834843b0254c4f7cd3
ashwin <ashwin.ramki96@gmail.com>
2022-08-30 17:23:12 -0500

    finished create a new spot route

--- backend/routes/api/spots.js
+++ backend/routes/api/spots.js
@@ -1,34 +1,84 @@
 const express = require('express');
 const { Spot, Review, SpotImage, sequelize} = require('../../db/models');
-const { Op } = require("sequelize");
+const { check } = require('express-validator');
+const { handleValidationErrors } = require('../../utils/validation');
 
 
 const router = express.Router();
 
+const validateSignup = [
+    check('address')
+      .exists({ checkFalsy: true })
+      .isString()
+      .withMessage('Please provide a valid address.'),
+    check('city')
+      .exists({ checkFalsy: true })
+      .isString()
+      .withMessage('Please provide a city.'),
+    check('state')
+      .exists({ checkFalsy: true })
+      .isString()
+      .withMessage('Please provide a state.'),
+    check('country')
+        .exists({ checkFalsy: true })
+        .isString()
+        .withMessage('Please provide a country'),
+    check('lat')
+        .exists({ checkFalsy: true })
+        .isFloat()
+        .withMessage('Please provide a valid latitude'),
+    check('lng')
+      .exists({ checkFalsy: true })
+      .isFloat()
+      .withMessage('Please provide a valid longitude'),
+    check('name')
+      .exists({ checkFalsy: true })
+      .isString()
+      .withMessage('Please provide a name'),
+    check('description')
+      .exists({ checkFalsy: true })
+      .isString()
+      .withMessage('Please provide a description'),
+    check('price')
+      .exists({ checkFalsy: true })
+      .isFloat()
+      .withMessage('Please provide a price'),
+    handleValidationErrors
+  ];
+
 router.get('/', async (req, res) => {
-    
-      
-      const spots = await Spot.findAll({
+
+
+    const spots = await Spot.findAll({
         group: ["Reviews.spotId", "SpotImages.url"],
         include: [{
             model: Review,
             attributes: []
         },
 
-        {   model: SpotImage,
+        {
+            model: SpotImage,
             attributes: ["url"]
-         }],
+        }],
         attributes: {
-        
-            include: [[sequelize.fn("AVG", sequelize.col("Reviews.stars")),"avgRating"]],
+
+            include: [[sequelize.fn("AVG", sequelize.col("Reviews.stars")), "avgRating"]],
         },
-        
-      });
-  
-      return res.json({
+
+    });
+
+    return res.json({
         spots,
-      });
-    }
-  );
+    });
+});
+
+router.post('/', validateSignup, async (req, res) => {
+    const {user} = req
+    const {address, city, state, country, lat, lng, name, description, price} = req.body
+    const newSpot = await Spot.create({
+        address, city, state, country, lat, lng, name, description, price, ownerId: user.id
+    });
+    return res.json(newSpot)
+})
 
   module.exports = router;
