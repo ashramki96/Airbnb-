b43c02cd6efd1b5800c4d607ec9ec36db119c118
ashwin <ashwin.ramki96@gmail.com>
2023-01-03 17:30:04 -0600

    adding css to search bar filters

--- frontend/src/components/SearchBar/index.js
+++ frontend/src/components/SearchBar/index.js
@@ -11,6 +11,7 @@ export default function SearchBar() {
     const [search, setSearch] = useState('')
     const [ratingFilter, setRatingFilter] = useState(0)
     const [priceFilter, setPriceFilter] = useState(5000)
+    const [showFilter, setShowFilter] = useState(false)
 
     useEffect(() => {
         dispatch(getSpots())
@@ -38,7 +39,12 @@ export default function SearchBar() {
 
     // const searchResults = filteredSpots(search, ratingFilter, priceFilter)
 
-    const filteredBySearch = spotsArr.filter(spot => spot.state === search)
+  
+    const filterSubmitHandler = async () => {
+        setShowFilter(!showFilter)
+    }
+
+    const filteredBySearch = spotsArr.filter(spot => spot.state.toUpperCase() === search.toUpperCase())
     const filteredByRating = filteredBySearch.filter(spot => spot.avgRating > ratingFilter)
     const filteredByPrice = filteredByRating.filter(spot => spot.price < priceFilter )
 
@@ -49,17 +55,41 @@ export default function SearchBar() {
     console.log("Filtered by Price", filteredByPrice)
     console.log("SEARCH RESULTS ARE", searchResults)
 
+
+
     return (
         // <div className = "search-and-results-container">
         <>
         <div className = "search-bar-container">
-            <div className='search-bar'>
-                <input className = 'search-text'
-                type = "search"
-                placeholder = "What state are you visiting?" 
-                value = {search}
-                onChange = {e => setSearch(e.target.value)}/>
-            </div>
+                <div className='search-bar'>
+                    <button className="filter-button" onClick={() => filterSubmitHandler()}><i class="fa fa-light fa-filter"></i></button>
+                    {showFilter && !search ?
+                        <div className="filter-dropdown">
+                        
+                        <div className = "filter-container-left">
+                          <div className = "filter-options">Under $200/night</div>
+                          <div className = "filter-options">Under $400/night</div>
+                          <div className = "filter-options">Under $500/night</div>
+                        </div>
+
+                        <div className = "filter-container-right">
+                          <div className = "filter-options">Over <i class="fa-sharp fa-solid fa-star fa-xs"></i>2</div>
+                          <div className = "filter-options">Over <i class="fa-sharp fa-solid fa-star fa-xs"></i>3</div>
+                          <div className = "filter-options">Over <i class="fa-sharp fa-solid fa-star fa-xs"></i>4</div>
+                        </div>
+
+
+
+                        </div> : null}
+
+                                        <input className='search-text'
+                                            type="search"
+                                            placeholder="What state are you visiting?"
+                                            value={search}
+                                            onChange={e => setSearch(e.target.value)} />
+                                        <button className="search-button" type="submit"><i class="fa fa-search"></i></button>
+
+                </div>
         </div>
 
         <div className = "results-container">
