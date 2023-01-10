9e6ce2f2d5db82669f9c56c3429ae82ec5998e6a
ashwin <ashwin.ramki96@gmail.com>
2023-01-10 12:35:17 -0600

    changes to booking seeder file

--- backend/db/seeders/20220901201458-demo-booking.js
+++ backend/db/seeders/20220901201458-demo-booking.js
@@ -1,46 +1,46 @@
-// 'use strict';
+'use strict';
 
-// module.exports = {
-//   async up (queryInterface, Sequelize) {
-//     /**
-//      * Add seed commands here.
-//      *
-//      * Example:
-//      * await queryInterface.bulkInsert('People', [{
-//      *   name: 'John Doe',
-//      *   isBetaMember: false
-//      * }], {});
-//     */
-//      await queryInterface.bulkInsert('Bookings', [
-//       {
-//         spotId: 1,
-//         userId: 2,
-//         startDate: Date ("2023-01-10"),
-//         endDate: Date("2023-01-15")
-//       },
-//       {
-//         spotId: 2,
-//         userId: 3,
-//         startDate: Date ("2023-02-10"),
-//         endDate: Date("2023-02-15")
-//       },
-//       {
-//         spotId: 3,
-//         userId: 2,
-//         startDate: Date("2023-03-10"),
-//         endDate: Date("2023-03-15")
-//       },
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    /**
+     * Add seed commands here.
+     *
+     * Example:
+     * await queryInterface.bulkInsert('People', [{
+     *   name: 'John Doe',
+     *   isBetaMember: false
+     * }], {});
+    */
+     await queryInterface.bulkInsert('Bookings', [
+      // {
+      //   spotId: 1,
+      //   userId: 2,
+      //   startDate: Date ("2023-01-10"),
+      //   endDate: Date("2023-01-15")
+      // },
+      // {
+      //   spotId: 2,
+      //   userId: 3,
+      //   startDate: Date ("2023-02-10"),
+      //   endDate: Date("2023-02-15")
+      // },
+      // {
+      //   spotId: 3,
+      //   userId: 2,
+      //   startDate: Date("2023-03-10"),
+      //   endDate: Date("2023-03-15")
+      // },
 
-//     ]);
-//   },
+    ]);
+  },
 
-//   async down (queryInterface, Sequelize) {
-//     /**
-//      * Add commands to revert seed here.
-//      *
-//      * Example:
-//      * await queryInterface.bulkDelete('People', null, {});
-//      */
-//      await queryInterface.bulkDelete('Bookings', null, {});
-//   }
-// };
+  async down (queryInterface, Sequelize) {
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+     await queryInterface.bulkDelete('Bookings', null, {});
+  }
+};
