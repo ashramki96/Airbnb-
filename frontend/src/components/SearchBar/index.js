e12e601bfec2befa3bf375bc4bf2ee26b3419a40
ashwin <ashwin.ramki96@gmail.com>
2023-01-20 15:57:47 -0600

    state abbreviations work with search

--- frontend/src/components/SearchBar/index.js
+++ frontend/src/components/SearchBar/index.js
@@ -64,9 +64,78 @@ export default function SearchBar() {
         setSearch("")
     }
 
-    const filteredBySearch = spotsArr.filter(spot => spot.state.toUpperCase().includes(search.toUpperCase()))
-    
-    const filteredByRating = filteredBySearch.filter(spot => spot.avgRating ? spot.avgRating > ratingFilter: spot.state.toUpperCase().includes(search.toUpperCase()))
+    let STATE_MAP = new Map()
+    STATE_MAP.set("AL", "Alabama");
+    STATE_MAP.set("AK", "Alaska");
+    STATE_MAP.set("AB", "Alberta");
+    STATE_MAP.set("AZ", "Arizona");
+    STATE_MAP.set("AR", "Arkansas");
+    STATE_MAP.set("BC", "British Columbia");
+    STATE_MAP.set("CA", "California");
+    STATE_MAP.set("CO", "Colorado");
+    STATE_MAP.set("CT", "Connecticut");
+    STATE_MAP.set("DE", "Delaware");
+    STATE_MAP.set("DC", "District Of Columbia");
+    STATE_MAP.set("FL", "Florida");
+    STATE_MAP.set("GA", "Georgia");
+    STATE_MAP.set("GU", "Guam");
+    STATE_MAP.set("HI", "Hawaii");
+    STATE_MAP.set("ID", "Idaho");
+    STATE_MAP.set("IL", "Illinois");
+    STATE_MAP.set("IN", "Indiana");
+    STATE_MAP.set("IA", "Iowa");
+    STATE_MAP.set("KS", "Kansas");
+    STATE_MAP.set("KY", "Kentucky");
+    STATE_MAP.set("LA", "Louisiana");
+    STATE_MAP.set("ME", "Maine");
+    STATE_MAP.set("MB", "Manitoba");
+    STATE_MAP.set("MD", "Maryland");
+    STATE_MAP.set("MA", "Massachusetts");
+    STATE_MAP.set("MI", "Michigan");
+    STATE_MAP.set("MN", "Minnesota");
+    STATE_MAP.set("MS", "Mississippi");
+    STATE_MAP.set("MO", "Missouri");
+    STATE_MAP.set("MT", "Montana");
+    STATE_MAP.set("NE", "Nebraska");
+    STATE_MAP.set("NV", "Nevada");
+    STATE_MAP.set("NB", "New Brunswick");
+    STATE_MAP.set("NH", "New Hampshire");
+    STATE_MAP.set("NJ", "New Jersey");
+    STATE_MAP.set("NM", "New Mexico");
+    STATE_MAP.set("NY", "New York");
+    STATE_MAP.set("NF", "Newfoundland");
+    STATE_MAP.set("NC", "North Carolina");
+    STATE_MAP.set("ND", "North Dakota");
+    STATE_MAP.set("NT", "Northwest Territories");
+    STATE_MAP.set("NS", "Nova Scotia");
+    STATE_MAP.set("NU", "Nunavut");
+    STATE_MAP.set("OH", "Ohio");
+    STATE_MAP.set("OK", "Oklahoma");
+    STATE_MAP.set("ON", "Ontario");
+    STATE_MAP.set("OR", "Oregon");
+    STATE_MAP.set("PA", "Pennsylvania");
+    STATE_MAP.set("PE", "Prince Edward Island");
+    STATE_MAP.set("PR", "Puerto Rico");
+    STATE_MAP.set("QC", "Quebec");
+    STATE_MAP.set("RI", "Rhode Island");
+    STATE_MAP.set("SK", "Saskatchewan");
+    STATE_MAP.set("SC", "South Carolina");
+    STATE_MAP.set("SD", "South Dakota");
+    STATE_MAP.set("TN", "Tennessee");
+    STATE_MAP.set("TX", "Texas");
+    STATE_MAP.set("UT", "Utah");
+    STATE_MAP.set("VT", "Vermont");
+    STATE_MAP.set("VI", "Virgin Islands");
+    STATE_MAP.set("VA", "Virginia");
+    STATE_MAP.set("WA", "Washington");
+    STATE_MAP.set("WV", "West Virginia");
+    STATE_MAP.set("WI", "Wisconsin");
+    STATE_MAP.set("WY", "Wyoming");
+    STATE_MAP.set("YT", "Yukon Territory");
+
+    const filteredBySearch = spotsArr.filter(spot => search && spot &&  ((spot.state.toUpperCase().startsWith(search.toUpperCase())) || (STATE_MAP.get(search.toUpperCase()) && (spot.state.toUpperCase() === STATE_MAP.get(search.toUpperCase()).toUpperCase()))))
+    // const filteredBySearch = spotsArr.filter(spot => spot.state.toUpperCase() === STATE_MAP.get(search.toUpperCase()).toUpperCase())
+    const filteredByRating = filteredBySearch.filter(spot => spot.avgRating ? spot.avgRating > ratingFilter: ((spot.state.toUpperCase().startsWith(search.toUpperCase())) || (STATE_MAP.get(search.toUpperCase()) && (spot.state.toUpperCase() === STATE_MAP.get(search.toUpperCase()).toUpperCase()))))
     const filteredByPrice = filteredByRating.filter(spot => spot.price < priceFilter )
 
     let searchResults = filteredByPrice
